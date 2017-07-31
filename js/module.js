24d0b86a380518bc2ff5373d95bd84ef411d32f5
Brajabandhu <das.brajabandhu@gmail.com>
2017-08-01 00:33:21 +0530

    changes

--- js/module.js
+++ js/module.js
@@ -2,12 +2,19 @@ var mymodule=angular.module("mymodule",[]).controller("mycontroller",function ($
     
 
         
-var employees={
-fname:"brajabandhu",
-lname: 'Das',
-gender:'male'
+var employees=[
+{fname:"brajabandhu",lname: "Das",gender:"male"    },
+{fname:"pappu",lname: "Das",gender:"male"    },
+{fname:"braja",lname: "Das",gender:"male"    },
+{fname:"braj",lname: "Das",gender:"male"    }
+];
+var technologies=[
+{name:"java",likes:0,dislikes:0},
+{name:"Spring",likes:0,dislikes:0},
+{name:"Hibernate",likes:0,dislikes:0},
+{name:"Angular",likes:0,dislikes:0},
 
-    }
+]
 var details={
     name:"brajabandhu",
     photo:"/image/Das.png",
@@ -15,5 +22,13 @@ var details={
 }
       
  $scope.details=details;
-$scope.employee=employees;   
+$scope.employees=employees; 
+$scope.technologies=technologies; 
+$scope.likes=function(tech){
+    tech.likes++;
+} 
+$scope.dislikes=function(tech){
+    tech.dislikes++;
+} 
+
 });
